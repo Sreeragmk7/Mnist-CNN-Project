4995d315ca5d19fea5bee7c6feefc6620980f6de
Sreeragmk7 <sreerag.mk@arts.christuniversity.in>
2025-11-19 07:25:34 +0530

    Update init_script.sh

--- init_script.sh
+++ init_script.sh
@@ -1,1 +1,16 @@
 echo "A script to create , activate and install requirements"
+echo "........"
+
+virtualenv venv
+
+echo " creation of virtualenv done!"
+echo " Activation my env"
+source venv/bin/activate
+
+echo "......."
+echo "Install requirement.txt"
+pip -install -r requirement.txt
+
+sleep(2)
+echo "Install Success!"
+echo "Creation , activation and installation of library is done!
